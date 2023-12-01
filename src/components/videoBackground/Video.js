ec105d8d08bb64839683423a329c207edea53bac
Yiyi41 <chenyy41@gmail.com>
2023-12-01 17:20:42 +0100

    fix cv link

--- src/components/videoBackground/Video.js
+++ src/components/videoBackground/Video.js
@@ -68,7 +68,7 @@ export default function Video() {
               </p>
               <div className={styles.btnWrapper}>
                 <Link
-                  href="https://drive.google.com/file/d/1HyYXAHqWxRDzZQxfFacaGbrM_5oiKtXD/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1gN_Ts-FO_jw115w18Nu39e3GKGo8MRqj/view?usp=sharing"
                   target="_blank"
                   className={styles.highlightCallBtn}
                 >
